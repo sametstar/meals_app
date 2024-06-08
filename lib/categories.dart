4c1472ac32292b4f55388a5329e6e5d4292785fe
sametstar <63130658+sametstar@users.noreply.github.com>
2024-06-09 01:29:22 +0300

    categories gridview

--- lib/categories.dart
+++ lib/categories.dart
@@ -10,9 +10,20 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Pick Your Category"),
       ),
       body: GridView(
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        children: [],
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20,
+          childAspectRatio: 3 / 2,
+        ),
+        children: const [
+          Text("1"),
+          Text("2"),
+          Text("3"),
+          Text("4"),
+          Text("5"),
+          Text("6"),
+        ],
       ),
     );
   }
