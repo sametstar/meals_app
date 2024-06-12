015f1075fe11b562604176682d49a3096618acea
sametstar <63130658+sametstar@users.noreply.github.com>
2024-06-12 16:52:06 +0300

    navigator function and inkwell

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category});
+  const CategoryGridItem(
+      {super.key, required this.category, required this.onSelectCategory});
 
   final Category category;
+  final void Function() onSelectCategory;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       // focusColor: Theme.of(context).focusColor,
       // hoverColor: Theme.of(context).hoverColor,
-      onTap: () {},
+      onTap: onSelectCategory,
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(16),
       child: Container(
