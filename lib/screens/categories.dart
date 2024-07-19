07ef12d2442720875def14c6852b273b2f20ba07
sametstar <63130658+sametstar@users.noreply.github.com>
2024-07-19 18:33:12 +0300

    passing function through multiple layers

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 import 'package:meals_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({super.key});
+  const CategoriesScreen({super.key, required this.onToggleFavorite});
+  final void Function(Meal meal) onToggleFavorite;
 
   void _selectCategory(BuildContext context, Category category) {
     final mealByCategory = dummyMeals
@@ -19,6 +21,7 @@ class CategoriesScreen extends StatelessWidget {
         builder: (ctx) => MealsScreen(
           title: category.title,
           meals: mealByCategory,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
