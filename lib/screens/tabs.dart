6b77550292c0e8fadf4a47fbf236f7cd7bc689f2
sametstar <63130658+sametstar@users.noreply.github.com>
2024-07-19 19:00:27 +0300

    set state added for fav list

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -16,9 +16,13 @@ class _TabsScreenState extends State<TabsScreen> {
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
     if (isExisting) {
-      _favoriteMeals.remove(meal);
+      setState(() {
+        _favoriteMeals.remove(meal);
+      });
     } else {
-      _favoriteMeals.add(meal);
+      setState(() {
+        _favoriteMeals.add(meal);
+      });
     }
   }
 
@@ -39,7 +43,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onToggleFavorite: _toggleMealFavoriteStatus,
       );
       activePageTitle = "Favorites";
